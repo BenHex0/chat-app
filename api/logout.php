1abafbaaca4b73ecf129186cbfded79b554815af
BenHex0 <194300233+BenHex0@users.noreply.github.com>
2026-01-04 20:11:27 +0200

    Add new files

---
+++ api/logout.php
@@ -0,0 +1,11 @@
+<?php
+header('Content-Type: application/json');
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+session_unset();
+session_destroy();
+
+echo json_encode(["status" => true, "message" => "Logged out"]);
+?>
